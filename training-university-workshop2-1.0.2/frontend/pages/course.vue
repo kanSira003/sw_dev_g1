ec8fea2f137558adec8454f4159e0c745dadda78
lebrancconvas <lebrancconvas@gmail.com>
2023-02-06 15:40:36 +0700

    ✨ feat: create front-end page (course and enroll)

---
+++ training-university-workshop2-1.0.2/frontend/pages/course.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>Course</h1>
+      </div>
+    </header>
+    <section id="course-section">
+      <v-form>
+        <v-text-field v-model="courseDetail.course_id" label="Course ID"></v-text-field>
+        <v-text-field v-model="courseDetail.course_name" label="Course Name"></v-text-field>
+        <v-text-field v-model="courseDetail.course_credit" label="Credit"></v-text-field>
+        <v-text-field v-model="courseDetail.course_maxseat" label="Max Seat"></v-text-field>
+        <v-btn color="error">Reset</v-btn>
+        <v-btn color="success">Add Course</v-btn>
+      </v-form>
+    </section>
+  </div>
+</template>
+
+<script>
+  // import axios from 'axios';
+  export default {
+    data() {
+      return {
+        courseDetail: {
+          course_id: 0,
+          course_name: "",
+          course_credit: 0,
+          course_maxseat: 0
+        }
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
---
+++ training-university-workshop2-1.0.2/frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>University Registration</h1>
+      </div>
+    </header>
+  </div>
+</template>
