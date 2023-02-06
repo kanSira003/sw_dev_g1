bbb5647bfe56ec9bcd53329a4e08c79f4b973c9a
lebrancconvas <lebrancconvas@gmail.com>
2023-02-06 16:12:31 +0700

    ✨ feat: add new course post api

--- training-university-workshop2-1.0.2/frontend/pages/course.vue
+++ training-university-workshop2-1.0.2/frontend/pages/course.vue
@@ -7,10 +7,10 @@
     </header>
     <section id="course-section">
       <v-form>
-        <v-text-field v-model="courseDetail.course_id" label="Course ID"></v-text-field>
-        <v-text-field v-model="courseDetail.course_name" label="Course Name"></v-text-field>
-        <v-text-field v-model="courseDetail.course_credit" label="Credit"></v-text-field>
-        <v-text-field v-model="courseDetail.course_maxseat" label="Max Seat"></v-text-field>
+        <v-text-field v-model="courseDetail.course_id" type="number" label="Course ID"></v-text-field>
+        <v-text-field v-model="courseDetail.course_name" type="text" label="Course Name"></v-text-field>
+        <v-text-field v-model="courseDetail.course_credit" type="number" label="Credit"></v-text-field>
+        <v-text-field v-model="courseDetail.course_maxseat" type="number" label="Max Seat"></v-text-field>
         <v-btn color="error" @click="reset">Reset</v-btn>
         <v-btn color="success" @click="addCourse">Add Course</v-btn>
       </v-form>
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-  // import axios from 'axios';
+  import axios from 'axios';
   export default {
     data() {
       return {
@@ -33,11 +33,26 @@
     },
     methods: {
       addCourse() {
+        const endpoint = "";
         console.log("Add Course");
         const isAllField = this.courseDetail.course_id !== 0 && this.courseDetail.course_name !== "" && this.courseDetail.course_detail !== 0 && this.courseDetail.course_maxseat !== 0;
 
         if(isAllField) {
           console.log("Add Course Success");
+          axios.post(endpoint, {
+            course_id: this.courseDetail.course_id,
+            course_name: this.courseDetail.course_name,
+            course_credit: this.courseDetail.course_credit,
+            course_maxseat: this.courseDetail.course_maxseat
+          })
+          .then(response => {
+            console.log(response);
+            console.log("[SUCCESS] Post Data Success.");
+          })
+          .catch(error => {
+            console.error(error);
+            console.log("[ERROR] Post Data Error.");
+          })
         } else {
           alert("Please fill in all the field");
         }
